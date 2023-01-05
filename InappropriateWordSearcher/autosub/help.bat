96326532af2d0126502649b1db26c11e05cab22e
zvz23 <ziegfredzorrilla23@gmail.com>
2023-01-05 09:32:16 +0800

    removed autosub

--- InappropriateWordSearcher/autosub/help.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set package_name=autosub
-@echo on
-%package_name% -h
-call cmd
--- InappropriateWordSearcher/autosub/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set package_name=autosub
-set "file_name="
-rem input your file name between '=' and '"'
-@echo on
-.\%package_name% -S en-US -i "%file_name%"
-call cmd
